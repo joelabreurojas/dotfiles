1cb3770d46dab7f922afebdaa693a2e168b46193
Joel Abreu Rojas <joelabreurojas@gmail.com>
2026-02-18 09:22:31 -0400

    feat: add bootstrap script for linux
    This script provides an automated way to initialize the dotfiles.
    It intelligently checks for existing `chezmoi` or `mise` installations
    before falling back to the official installation script, ensuring
    a smooth setup process on fresh systems.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SOURCE="https://github.com/joelabreurojas/dotfiles.git"
+
+if command -v chezmoi >/dev/null; then
+	chezmoi init --apply $SOURCE
+	exit 0
+fi
+
+if command -v mise >/dev/null; then
+	mise use -g chezmoi && mise exec chezmoi -- chezmoi init --apply $SOURCE
+	exit 0
+fi
+
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $SOURCE
+exit 0
