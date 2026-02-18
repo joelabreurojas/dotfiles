383fa3761e43a900df842b3637277062fd8b621e
Joel Abreu Rojas <joelabreurojas@gmail.com>
2026-02-18 09:40:01 -0400

    feat: add bootstrap script for windows
    This script provides an automated way to initialize the dotfiles on
    Windows systems. It intelligently checks for existing `chezmoi` or
    `mise` installations before falling back to the official installer,
    ensuring a smooth setup process on fresh systems.

---
+++ install.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = "Stop"
+$SOURCE = "https://github.com/joelabreurojas/dotfiles.git"
+
+if (Get-Command chezmoi -ErrorAction SilentlyContinue) {
+    chezmoi init --apply $SOURCE
+    exit
+}
+
+if (Get-Command mise -ErrorAction SilentlyContinue) {
+    mise use -g chezmoi && mise exec chezmoi -- chezmoi init --apply $SOURCE
+    exit
+}
+
+irm https://get.chezmoi.io/ps1 | iex && chezmoi init --apply $SOURCE
