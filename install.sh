c6a86cb8193fa47cf99d5176cbbbe496e9a37803
Joel Abreu Rojas <joelabreurojas@gmail.com>
2026-08-05 15:54:51 -0400

    fix(install): pin branch, fix includes, drop cargo

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 set -euo pipefail
 
 SOURCE="https://github.com/joelabreurojas/dotfiles.git"
+BRANCH="omarchy"
 
 if command -v chezmoi >/dev/null; then
-	chezmoi init --apply $SOURCE
+	chezmoi init --apply --branch="$BRANCH" $SOURCE
 	exit 0
 fi
 
 if command -v mise >/dev/null; then
-	mise use -g -q && chezmoi init --apply $SOURCE
+	mise use -g -q && chezmoi init --apply --branch="$BRANCH" $SOURCE
 	exit 0
 fi
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $SOURCE
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --branch="$BRANCH" $SOURCE
 exit 0
